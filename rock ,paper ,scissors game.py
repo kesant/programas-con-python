349ac1b6976217f4d7df9a2db474927ae9d3e7dc
kesant <kevin_santana120@hotmail.com>
2022-05-24 23:05:49 -0500

    we created a rock ,paper, scissor game giving numbers to each item for the player to chose and the computer will chose one randomly and the program will determinate who is the winner

---
+++ rock ,paper ,scissors game.py
@@ -0,0 +1,32 @@
+#rock ,paper ,scissors game 
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+print("*"*25)
+player=input("What is your name? : ")
+print(f"Welcome {player} to rock paper scissors game") 
+print("*"*25)
+print("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.")
