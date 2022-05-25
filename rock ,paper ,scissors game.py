1f84a99e35a059ddfd8d935c5818efe823646f6e
kesant <kevin_santana120@hotmail.com>
2022-05-24 23:36:28 -0500

    we designed a system that shows the player choice and the computer choice and print who is the winner , using conditional statements if elif and else also using the module random to chose the computer choice randomly

--- rock ,paper ,scissors game.py
+++ rock ,paper ,scissors game.py
@@ -1,4 +1,5 @@
 #rock ,paper ,scissors game 
+import random
 rock = '''
     _______
 ---'   ____)
@@ -25,8 +26,24 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+items=[0,1,2]
+item=[rock,paper,scissors]
 print("*"*25)
 player=input("What is your name? : ")
 print(f"Welcome {player} to rock paper scissors game") 
 print("*"*25)
-print("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.")
+choice=int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors."))
+computer_choice=random.choice(items)
+print(item[choice])
+print("Computer choice :")
+print(item[computer_choice])
+if choice==0 and computer_choice==2:
+    print("You win")
+elif choice==1 and computer_choice==0:
+    print("You win")
+elif choice==2 and computer_choice==1:
+    print("You win")
+elif choice==computer_choice:
+    print("No winner")
+else:
+    print("computer wins")
