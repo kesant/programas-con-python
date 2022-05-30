d35281f353de10fc0bb405343b3930f31e93f166
kevin <kejosant@espol.edu.ec>
2022-05-29 23:56:46 -0500

    we added changes

--- ayudantia.py
+++ ayudantia.py
@@ -1,6 +1,7 @@
 #Crea un programa usando matemáticas y f-Strings que nos diga cuántos días, semanas, meses nos quedan si vivimos hasta los 90 años.
 #Tomará su edad actual como entrada y salida de un mensaje con nuestro tiempo restante en este formato:
 #Te quedan x días, y semanas y z meses.
+
 age=input("Cual es tu edad actual ? : ")
 age_as_int=int(age)
 years_remaining= 90-age_as_int
