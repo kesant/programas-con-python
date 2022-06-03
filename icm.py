aa94a5a29edd4e8d940037cd4e96d2996713d370
kesant <kevin_santana120@hotmail.com>
2022-06-02 23:57:40 -0500

    we added comments explaining the requirements of the problem

--- icm.py
+++ icm.py
@@ -1,3 +1,4 @@
+#programa de como calcular el indice de masa corporal
 #Escriba un programa que interprete el índice de masa corporal (IMC) en función del peso y la altura de un usuario.
 #Debe decirles la interpretación de su IMC en función del valor de IMC:
 #Menos de 18,5 tienen bajo peso
@@ -5,10 +6,14 @@
 ##Por encima de los 25 pero por debajo de los 30 tienen un ligero sobrepeso
 #Mayores de 30 pero menores de 35 son obesos
 ##Por encima de 35 son clínicamente obesos.
+
 altura =float(input("Ingresa tu altura m: "))
+
 peso=float(input("Ingresa tu peso en kg :"))
+
 imc=round((peso/altura**2),2)
 print(imc)
+
 if imc <18.5:
    print(f"Tu IMC es {imc}, tienes bajo peso.")
 elif imc<25 :
